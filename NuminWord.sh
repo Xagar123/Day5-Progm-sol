ce8c7ba45ace800402b056e4da131e4c0e806f11
Xagar123 <sagarcoc2000@gmail.com>
2022-05-28 12:54:00 +0530

    New file added

---
+++ NuminWord.sh
@@ -0,0 +1,36 @@
+read -p "Enter a single digit number : " Num
+if [ $Num -eq 0 ];
+then
+	echo "Zero";
+elif [ $Num -eq 1 ];
+then 
+	echo "One";
+elif [ $Num -eq 2 ];
+then
+        echo "Two";
+elif [ $Num -eq 3 ];
+then
+        echo "Three";
+elif [ $Num -eq 4 ];
+then
+        echo "Four";
+elif [ $Num -eq 5 ];
+then
+        echo "Five";
+elif [ $Num -eq 6 ];
+then
+        echo "Six";
+elif [ $Num -eq 7 ];
+then
+        echo "Seven";
+elif [ $Num -eq 8 ];
+then
+        echo "Eight";
+elif [ $Num -eq 9 ];
+then
+        echo "Nine";
+else
+	echo "Wrong number input"
+fi
+
+
